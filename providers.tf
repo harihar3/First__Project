48afaf9f694d854237eae25d7926db4b546c5779
Harihar Panda <harihar.panda1@kyndryl.com>
2025-01-23 14:09:34 +0530

    commiting code

---
+++ providers.tf
@@ -0,0 +1,25 @@
+
+terraform {
+  required_version = "1.3.9"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.12"
+    }
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Using remote backend
+/*
+terraform {
+  backend "s3" {
+    bucket = "my-backend-devops101-terraform"
+    key    = "flaskappawsec2/tfstate"
+    region = "ap-south-1"
+  }
+}
+*/
